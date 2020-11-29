e6e27d1acdaf0e52a86186fc563361ce283a8259
JoanSebastianGomez <joan.gomez@udea.edu.co>
2020-11-28 23:18:32 -0500

    add some things

--- saxpy.c
+++ saxpy.c
@@ -17,85 +17,120 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <sys/time.h>
+#include <pthread.h>
 
-int main(int argc, char* argv[]){
-	// Variables to obtain command line parameters
-	unsigned int seed = 1;
-  	int p = 10000000;
-  	int n_threads = 2;
-  	int max_iters = 1000;
-  	// Variables to perform SAXPY operation
-	double* X;
-	double a;
-	double* Y;
-	double* Y_avgs;
-	int i, it;
-	// Variables to get execution time
-	struct timeval t_start, t_end;
-	double exec_time;
+// Variables to obtain command line parameters
+unsigned int seed = 1;
+int p = 10000000;
+int n_threads = 2;
+int max_iters = 1000;
+// Variables to perform SAXPY operation
+double *X;
+double a;
+double *Y;
+double *Y_avgs;
+int i, it;
+// Variables to get execution time
+struct timeval t_start, t_end;
+double exec_time;
+
+void *calcular(void *arg)
+{
+	for (it = 0; it < max_iters; it++)
+	{
+		for (i = 0; i < p; i++)
+		{
+			Y[i] = Y[i] + a * X[i];
+			Y_avgs[it] += Y[i];
+		}
+		Y_avgs[it] = Y_avgs[it] / p;
+	}
+	return NULL;
+}
+
+void *twoThreads(void *arg)
+{
+	pthread_t h1;
+	pthread_t h2;
+	pthread_create(&h1, NULL, calcular, NULL);
+	pthread_create(&h2, NULL, calcular, NULL);
+	pthread_join(h1, NULL);
+	pthread_join(h2, NULL);
+	return NULL;
+
+}
+
+int main(int argc, char *argv[])
+{
 
 	// Getting input values
 	int opt;
-	while((opt = getopt(argc, argv, ":p:s:n:i:")) != -1){  
-		switch(opt){  
-			case 'p':  
+	while ((opt = getopt(argc, argv, ":p:s:n:i:")) != -1)
+	{
+		switch (opt)
+		{
+		case 'p':
 			printf("vector size: %s\n", optarg);
 			p = strtol(optarg, NULL, 10);
 			assert(p > 0 && p <= 2147483647);
-			break;  
-			case 's':  
+			break;
+		case 's':
 			printf("seed: %s\n", optarg);
 			seed = strtol(optarg, NULL, 10);
 			break;
-			case 'n':  
+		case 'n':
 			printf("threads number: %s\n", optarg);
 			n_threads = strtol(optarg, NULL, 10);
-			break;  
-			case 'i':  
+			break;
+		case 'i':
 			printf("max. iterations: %s\n", optarg);
 			max_iters = strtol(optarg, NULL, 10);
-			break;  
-			case ':':  
-			printf("option -%c needs a value\n", optopt);  
-			break;  
-			case '?':  
+			break;
+		case ':':
+			printf("option -%c needs a value\n", optopt);
+			break;
+		case '?':
 			fprintf(stderr, "Usage: %s [-p <vector size>] [-s <seed>] [-n <threads number>]\n", argv[0]);
 			exit(EXIT_FAILURE);
-		}  
-	}  
+		}
+	}
 	srand(seed);
 
-	printf("p = %d, seed = %d, n_threads = %d, max_iters = %d\n", \
-	 p, seed, n_threads, max_iters);	
+	printf("p = %d, seed = %d, n_threads = %d, max_iters = %d\n",
+		   p, seed, n_threads, max_iters);
 
 	// initializing data
-	X = (double*) malloc(sizeof(double) * p);
-	Y = (double*) malloc(sizeof(double) * p);
-	Y_avgs = (double*) malloc(sizeof(double) * max_iters);
+	X = (double *)malloc(sizeof(double) * p);
+	Y = (double *)malloc(sizeof(double) * p);
+	Y_avgs = (double *)malloc(sizeof(double) * max_iters);
 
-	for(i = 0; i < p; i++){
+	for (i = 0; i < p; i++)
+	{
 		X[i] = (double)rand() / RAND_MAX;
 		Y[i] = (double)rand() / RAND_MAX;
 	}
-	for(i = 0; i < max_iters; i++){
+	for (i = 0; i < max_iters; i++)
+	{
 		Y_avgs[i] = 0.0;
 	}
 	a = (double)rand() / RAND_MAX;
 
 #ifdef DEBUG
 	printf("vector X= [ ");
-	for(i = 0; i < p-1; i++){
-		printf("%f, ",X[i]);
+	for (i = 0; i < p - 1; i++)
+	{
+		printf("%f, ", X[i]);
 	}
-	printf("%f ]\n",X[p-1]);
+	printf("%f ]\n", X[p - 1]);
 
 	printf("vector Y= [ ");
-	for(i = 0; i < p-1; i++){
+	for (i = 0; i < p - 1; i++)
+	{
 		printf("%f, ", Y[i]);
 	}
-	printf("%f ]\n", Y[p-1]);
+	printf("%f ]\n", Y[p - 1]);
 
-	printf("a= %f \n", a);	
+	printf("a= %f \n", a);
 #endif
 
 	/*
@@ -103,28 +138,28 @@ int main(int argc, char* argv[]){
 	 */
 	gettimeofday(&t_start, NULL);
 	//SAXPY iterative SAXPY mfunction
-	for(it = 0; it < max_iters; it++){
-		for(i = 0; i < p; i++){
-			Y[i] = Y[i] + a * X[i];
-			Y_avgs[it] += Y[i];
-		}
-		Y_avgs[it] = Y_avgs[it] / p;
-	}
+
+	//hilos aca
+	twoThreads(NULL);
+
+	// hilos finaliza
+
 	gettimeofday(&t_end, NULL);
 
 #ifdef DEBUG
 	printf("RES: final vector Y= [ ");
-	for(i = 0; i < p-1; i++){
+	for (i = 0; i < p - 1; i++)
+	{
 		printf("%f, ", Y[i]);
 	}
-	printf("%f ]\n", Y[p-1]);
+	printf("%f ]\n", Y[p - 1]);
 #endif
-	
+
 	// Computing execution time
-	exec_time = (t_end.tv_sec - t_start.tv_sec) * 1000.0;  // sec to ms
+	exec_time = (t_end.tv_sec - t_start.tv_sec) * 1000.0;	 // sec to ms
 	exec_time += (t_end.tv_usec - t_start.tv_usec) / 1000.0; // us to ms
 	printf("Execution time: %f ms \n", exec_time);
-	printf("Last 3 values of Y: %f, %f, %f \n", Y[p-3], Y[p-2], Y[p-1]);
-	printf("Last 3 values of Y_avgs: %f, %f, %f \n", Y_avgs[max_iters-3], Y_avgs[max_iters-2], Y_avgs[max_iters-1]);
+	printf("Last 3 values of Y: %f, %f, %f \n", Y[p - 3], Y[p - 2], Y[p - 1]);
+	printf("Last 3 values of Y_avgs: %f, %f, %f \n", Y_avgs[max_iters - 3], Y_avgs[max_iters - 2], Y_avgs[max_iters - 1]);
 	return 0;
-}	
+}
